9abd1950bac274ccacf7499a738eb704e4694094
Matteo Grammatico <rosarioma.grammatico@studenti.unicam.it>
2020-01-14 01:32:14 +0100

    Update interfaces.c

--- interfaces.c
+++ interfaces.c
@@ -15,31 +15,6 @@ void set_up(){
 int key_counter = 0;
 GTimer *gtimer;
 glong microsec;
-//void activate(GtkApplication* app, gpointer user_data){
-	/** Initializing window **/
-//	GtkWidget *window;
-//	window = gtk_application_window_new(app);
-	//gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
-//	g_signal_connect (window, "key-release-event", G_CALLBACK (my_keypress_function), NULL);
-	/*
-	gtk_window_set_title(GTK_WINDOW(window), "COSENZA");
-	gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-	gtk_widget_show_all(window);
-	*/
-	/** Initializing button **/
-	/*
-	GtkWidget *button, *button_box;
-	button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-	gtk_container_add (GTK_CONTAINER (window), button_box);
-	button = gtk_button_new_with_label ("COSENZA");
-	g_signal_connect (button, "clicked", G_CALLBACK (print_on_window), NULL);
-	g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
-	gtk_container_add (GTK_CONTAINER (button_box), button);
-
-	gtk_widget_show_all (window);
-	* */
-//}
-
 
 gboolean my_keypress_function(GtkWidget *widget, GdkEventKey *event, gpointer data){
     key_counter++;
